4530973fc530536df04eef59a9e55a81b749e322
Timothy Hunt <hunttim001@warren.k12.in.us>
2024-02-06 09:53:26 -0500

    gang

--- 01_Strings/05_Search Strings/notes.py
+++ 01_Strings/05_Search Strings/notes.py
@@ -1,63 +1,86 @@
-def min(a : int, b: int) -> int:
-    if(a < b):
-        return a
-    else:
-        return b
-def print_min(a: int, b: int) -> None:
-    if (a < b):
-        print(a)
-    else:
-        print(b)   
+# # from sys import *
+# # from sys import exit
+# # import sys
 
-num1: int = int(input("Enter a number: "))
-num2 : int = int(input("Enter another number: "))
-# print(min(num1, num2))
+# # import sys
+# # import math
+# # import sys
+# # import math
+# # import sys
 
-result = print_min(num1, num2)
-print(result)
+# # for name in dir(sys):
+# #     print(name, end='\t')
+# # sys.exit()
+
+# # sys.exit324()
 
 
+# # def exit():
+# #     print('I wanna exit')
 
 
+# # exit()
+# # sys.exit()
+
+# # exit()
 
 
+# # def exit():
+# #     print('I wanna exit')
 
 
+# # exit()
+# # from sys import *
+# from sys import exit as bye_bye
+# import sys  # you need to add sys.
+# from sys import *
+# import sys as s
 
+# s.exit()
 
+# bye_bye()
 
+import math
 
+for name in dir(math):
+    print(name, end='\t')
+    
+# ceil(), floor(), trunc()
+print(math.ceil(3.6))
+print(math.floor(3.6))
+print(math.trunc(3.6))
 
+# ceil()
+print(math.ceil(3.1))
+print(math.ceil(3.505))
+print(math.ceil(3.945))
+print(math.ceil(3.0))
+print(math.ceil(-5.4))
 
+# floor()
+print(math.floor(3.1))
+print(math.floor(3.505))
+print(math.floor(3.945))
+print(math.floor(3.0))
+print(math.floor(-5.4))
 
+# trunc()
+print(math.trunc(3.1))
+print(math.trunc(3.505))
+print(math.trunc(3.945))
+print(math.trunc(3.0))
+print(math.trunc(-5.4))
 
+# factorial()
+# 3! = 3 * 2 * 1 = 6
+# 4! = 4 * 3 * 2 * 1 = 24
+print(math.factorial(3))
+print(math.factorial(4))
 
+# sqrt()
+print(math.sqrt(16))
+print(math.sqrt(100))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def min(a: int, b : int) -> int:
-    if (a < b):
-        return a
-    else:
-        return b
-
-
-
-
-
-number1: int = int(input("Enter a number"))
-number2: int = int(input("Enter another number"))
-print(min(number1, number2))
+# hypot()
+print(math.hypot(3, 4))
 
